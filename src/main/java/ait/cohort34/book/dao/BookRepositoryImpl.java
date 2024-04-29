579b0020c15462499c4e80f68bc0c612927229ce
VK <vit_ko@ukr.net>
2024-04-29 11:07:17 +0200

    Complete implementation of all methods in repositories using EntityManager.

--- src/main/java/ait/cohort34/book/dao/BookRepositoryImpl.java
+++ src/main/java/ait/cohort34/book/dao/BookRepositoryImpl.java
@@ -3,6 +3,7 @@ package ait.cohort34.book.dao;
 import ait.cohort34.book.model.Book;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -17,22 +18,38 @@ public class BookRepositoryImpl implements BookRepository {
 
     @Override
     public Stream<Book> findByAuthorsName(String authorName) {
-        return em.createQuery("SELECT b FROM Book b JOIN b.authors a WHERE a.name = :authorName", Book.class)
-                .setParameter("authorName", authorName)
+//        return em.createQuery("SELECT b FROM Book b JOIN b.authors a WHERE a.name = :authorName", Book.class)
+//                .setParameter("authorName", authorName)
+//                .getResultStream();
+
+        return em.createQuery("SELECT b FROM Book b JOIN b.authors a WHERE a.name = ?1", Book.class)
+                .setParameter(1, authorName)
                 .getResultStream();
     }
 
     @Override
     public Stream<Book> findByPublisherPublisherName(String publisherName) {
-        return em.createQuery("SELECT b FROM Book b WHERE b.publisher.publisherName = :publisherName", Book.class)
-                .setParameter("publisherName", publisherName)
+//        return em.createQuery("SELECT b FROM Book b WHERE b.publisher.publisherName = :publisherName", Book.class)
+//                .setParameter("publisherName", publisherName)
+//                .getResultStream();
+
+        return em.createQuery("SELECT b FROM Book b join b.publisher p where p.publisherName =?1", Book.class)
+                .setParameter(1, publisherName)
                 .getResultStream();
     }
 
     @Override
     public void deleteByAuthorsName(String authorName) {
-        em.createQuery("DELETE FROM Book b WHERE :authorName IN (SELECT a.name FROM b.authors a)")
-                .setParameter("authorName", authorName)
+//        em.createQuery("DELETE FROM Book b WHERE :authorName IN (SELECT a.name FROM b.authors a)")
+//                .setParameter("authorName", authorName)
+//                .executeUpdate();
+
+//        Query query = em.createQuery("DELETE FROM Book b join b.authors a WHERE a.name = ?1");
+//                query.setParameter(1, authorName);
+//                query.executeUpdate();
+
+        em.createQuery("delete from Book b where :name member of b.authors")
+                .setParameter("name", authorName)
                 .executeUpdate();
     }
 
--- src/main/java/ait/cohort34/book/dao/PublisherRepositoryImpl.java
+++ src/main/java/ait/cohort34/book/dao/PublisherRepositoryImpl.java
@@ -1,15 +1,9 @@
 package ait.cohort34.book.dao;
 
-import ait.cohort34.book.model.Author;
-import ait.cohort34.book.model.Book;
 import ait.cohort34.book.model.Publisher;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Join;
-import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -25,18 +19,26 @@ public class PublisherRepositoryImpl implements PublisherRepository {
 
     @Override
     public List<String> findByPublishersAuthor(String authorName) {
-        String jpql = "SELECT DISTINCT p.publisherName FROM Publisher p JOIN p.books b JOIN b.authors a WHERE a.name = :authorName";
-        return em.createQuery(jpql, String.class)
-                .setParameter("authorName", authorName)
+//        String jpql = "SELECT DISTINCT p.publisherName FROM Publisher p JOIN p.books b JOIN b.authors a WHERE a.name = :authorName";
+//        return em.createQuery(jpql, String.class)
+//                .setParameter("authorName", authorName)
+//                .getResultList();
+
+        return em.createQuery("select distinct p.publisherName from Book b join b.publisher p join b.authors a where a.name =?1")
+                .setParameter(1, authorName)
                 .getResultList();
     }
 
     @Override
     public Stream<Publisher> findDistinctByBooksAuthorsName(String authorName) {
-        String jpql = "SELECT DISTINCT p FROM Publisher p JOIN p.books b JOIN b.authors a WHERE a.name = :authorName";
-        return em.createQuery(jpql, Publisher.class)
-                .setParameter("authorName", authorName)
-                .getResultStream();
+//        String jpql = "SELECT DISTINCT p FROM Publisher p JOIN p.books b JOIN b.authors a WHERE a.name = :authorName";
+//        return em.createQuery(jpql, Publisher.class)
+//                .setParameter("authorName", authorName)
+//                .getResultStream();
+
+        TypedQuery<Publisher> query = em.createQuery("SELECT DISTINCT p FROM Book b JOIN b.publisher p join b.authors a where a.name =?1", Publisher.class);
+        query.setParameter(1, authorName);
+        return query.getResultStream();
     }
 
     @Override
@@ -46,8 +48,8 @@ public class PublisherRepositoryImpl implements PublisherRepository {
 
     @Override
     public Publisher save(Publisher publisher) {
-//        em.persist(publisher);
-        em.merge(publisher);
+        em.persist(publisher);
+//        em.merge(publisher);
         return publisher;
     }
 }
